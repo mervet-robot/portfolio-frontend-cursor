175acbb04f148e70faca49646533a45b62363f42
mervet-robot <mervetrobot@gmail.com>
2025-05-22 22:34:01 +0200

    Fix erreur jwt-windows

--- src/app/_services/token.service.ts
+++ src/app/_services/token.service.ts
@@ -11,32 +11,45 @@ export class TokenService {
   private readonly TOKEN_KEY = 'auth-token';
   private readonly USER_KEY = 'auth-user';
 
-  constructor() {}
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
   saveToken(token: string): void {
-    window.sessionStorage.removeItem(this.TOKEN_KEY);
-    window.sessionStorage.setItem(this.TOKEN_KEY, token);
+    if (isPlatformBrowser(this.platformId)) {
+      window.sessionStorage.removeItem(this.TOKEN_KEY);
+      window.sessionStorage.setItem(this.TOKEN_KEY, token);
+    }
   }
 
   getToken(): string | null {
-    return window.sessionStorage.getItem(this.TOKEN_KEY);
+    if (isPlatformBrowser(this.platformId)) {
+      return window.sessionStorage.getItem(this.TOKEN_KEY);
+    }
+    return null;
   }
 
   signOut(): void {
-    window.sessionStorage.clear();
+    if (isPlatformBrowser(this.platformId)) {
+      window.sessionStorage.clear();
+    }
   }
 
   saveUser(user: any): void {
-    window.sessionStorage.removeItem(this.USER_KEY);
-    window.sessionStorage.setItem(this.USER_KEY, JSON.stringify(user));
+    if (isPlatformBrowser(this.platformId)) {
+      window.sessionStorage.removeItem(this.USER_KEY);
+      window.sessionStorage.setItem(this.USER_KEY, JSON.stringify(user));
+    }
   }
 
   getUser(): any {
-    const user = window.sessionStorage.getItem(this.USER_KEY);
-    return user ? JSON.parse(user) : null;
+    if (isPlatformBrowser(this.platformId)) {
+      const user = window.sessionStorage.getItem(this.USER_KEY);
+      return user ? JSON.parse(user) : null;
+    }
+    return null;
   }
 
   isTokenExpired(token: string): boolean {
+    if (!token) return true;
     const expiry = (JSON.parse(atob(token.split('.')[1]))).exp;
     return (Math.floor((new Date).getTime() / 1000)) >= expiry;
   }
